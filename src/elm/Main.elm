929ce64f6585d8dd0f4b55228fbac1a0bcd26fa8
pfcm <pfcmathews@gmail.com>
2017-11-20 23:51:26 +1300

    add a little info

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -73,10 +73,41 @@ getHexClass model flatIndex =
                 "hex-empty"
 
 
+playerName : Game.Player -> String
+playerName player =
+    case player of
+        Game.Player1 ->
+            "Player 1"
+
+        Game.Player2 ->
+            "Player 2"
+
+
+playerClass : Game.Player -> String
+playerClass player =
+    case player of
+        Game.Player1 ->
+            "player1"
+
+        Game.Player2 ->
+            "player2"
+
+
+turnBox : Model -> Html msg
+turnBox model =
+    div [ class "turn-box" ]
+        [ div [ class << playerClass <| model.gameState.nextTurn ]
+            [ text ((playerName model.gameState.nextTurn) ++ "'s ")
+            ]
+        , text "turn."
+        ]
+
+
 view : Model -> Html Msg
 view model =
     div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
-        [ hexagonGrid 11 GridClick (getHexClass model)
+        [ turnBox model
+        , hexagonGrid 5 GridClick (getHexClass model)
         ]
 
 
