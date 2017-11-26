619aceea02f6ba907ceaa64a35c94c0dd35dc92d
pfcm <pfcmathews@gmail.com>
2017-11-26 21:19:58 +1300

    tidy up a little bit

--- src/elm/GraphSearch.elm
+++ src/elm/GraphSearch.elm
@@ -1,7 +1,8 @@
 module GraphSearch exposing (dfs, SearchResult, Neighbourhood)
 
-{-| Minimal set of graph search algorithms, designed to be as representation
-agnostic as possible. Leads to some functions with a large number of arguments.
+{-| Minimal set of graph search algorithms, designed to be as
+representation agnostic as possible. Leads to some functions with
+a large number of arguments.
 
 
 # Functions
@@ -22,23 +23,41 @@ type alias SearchResult a =
     ( Set a, List a )
 
 
-{-| Basic depth-first search. Requires three arguments: a set of elements already
-visited, a function to take an element and generate its neighbours and a
-starting position. The result is the updated set of visted elements and a
-list of all the nodes visited (pre-order).
+{-| Basic depth-first search. Requires three arguments: a set of
+elements already visited, a function to take an element and
+generate its neighbours and a starting position. The result is the
+updated set of visted elements and a list of all the nodes visited
+(pre-order).
 
-It's up to the caller to supply the neighbourhood function, this implicitly
-defines the whole graph -- how it is done can have a strong effect on the
-algorithmic complexity of the whole thing so be a little bit careful.
+It's up to the caller to supply the neighbourhood function, this
+implicitly defines the whole graph -- how it is done can have a
+strong effect on the algorithmic complexity of the whole thing so
+be a little bit careful.
 
 -}
-dfs : Set comparable -> Neighbourhood comparable -> comparable -> SearchResult comparable
+dfs :
+    Set comparable
+    -> Neighbourhood comparable
+    -> comparable
+    -> SearchResult comparable
 dfs visited neighbours start =
     List.foldl (dfsFold neighbours) ( visited, [] ) <|
-        (List.filter (not << flip Set.member visited) <| neighbours start)
+        (List.filter
+            (not
+                << flip Set.member visited
+            )
+         <|
+            neighbours start
+        )
 
 
-dfsFold : Neighbourhood comparable -> (comparable -> SearchResult comparable -> SearchResult comparable)
+dfsFold :
+    Neighbourhood comparable
+    ->
+        (comparable
+         -> SearchResult comparable
+         -> SearchResult comparable
+        )
 dfsFold neighbours =
     let
         foldInner val ( visited, path ) =
